26483d182f3f27030c4d698773a17df198325014
Chun-Hui Gao <gaospecial@gmail.com>
2026-03-23 13:36:35 +0800

    模型与mps不兼容

--- train.py
+++ train.py
@@ -27,8 +27,6 @@ def set_seed(seed=42):
 def get_device():
     if torch.cuda.is_available():
         return torch.device("cuda")
-    elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
-        return torch.device("mps")
     else:
         return torch.device("cpu")
 
